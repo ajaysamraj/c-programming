f1f1a639b9932c44747ad60d886deff1cea19a05
ajaysamraj <34670366+ajaysamraj@users.noreply.github.com>
2017-12-19 10:55:53 +0530

    Create var.c

---
+++ beginner/var.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+void main()
+{
+ char a, b;
+	scanf("%c",a);
+    printf("name%c",a);
+    scanf("%c",b);
+    printf("Nick name%c",b);
+   int c,e;
+	scanf("%d",c);
+    printf("no of runs%d",c);
+   float d;
+	scanf("%f",d);
+    printf("batting avaerage%f",d);
+	scanf("%d",e);
+    printf("no of matches%d",e);
+}
